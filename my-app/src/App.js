562037f9ca03a75d6a1af2c6c256d7057d75fe77
parlier.valentin <parlier.valentin@gmail.com>
2020-10-15 17:03:20 +0200

    Structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,55 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-import { Grommet, Header, Button, Menu } from 'grommet';
-import { withStyles } from '@material-ui/core/styles';
+import { Grommet, Header, Button, Menu, Footer, Text, Anchor, Main, Heading, Paragraph, Avatar, Box, Grid } from 'grommet';
+import { Home, UserFemale } from 'grommet-icons';
+
 
 
 function App() {
   return (
     <div className="App">
-    <Header background="brand">
-  <Button icon={<Icons.Home />} hoverIndicator />
-  <Menu label="account" items={[{ label: 'logout' }]} />
-</Header>
+      <Header background="#2c3e50">
+        <Button margin="0,1em" icon={<Avatar src="https://pbs.twimg.com/profile_images/554072268004147200/Fuos0cpV.jpeg" />} hoverIndicator />
+        <Heading size="xsmall" textAlign="end" >Valentin Parlier</Heading>
+        <Menu label="Menu" items={[{ label: 'Mon CV', href: 'https://www.google.com' }, {label: 'Me contacter'}, {label: 'Me faire un don <3'}]} />
+      </Header>
+
+
+
+      <Grid
+        rows={['xxsmall', 'medium', 'xsmall']}
+        columns={['1/4', '2,999999/4']}
+        areas={[
+          ['header', 'header'],
+          ['sidebar', 'main'],
+          ['footer', 'footer'],
+        ]}
+        gap="small"
+      >
+        <Box background="brand" gridArea="header">
+          Hello
+        </Box>
+
+        <Box background="light-5" gridArea="sidebar">
+          Sidebar
+        </Box>
+
+        <Box background="light-2" gridArea="main">
+          Main
+          <Paragraph>Tu te trouves actuellement sur mon site CV. C'est ici que tu pourra retrouver quelques-uns de mes projets ainsi qu'une description de mon parcours professionnel.</Paragraph>
+
+        </Box>
+
+        <Box background="dark-2" gridArea="footer">
+          Footer
+        </Box>
+      </Grid>
+
+      <Footer background="#2c3e50" pad="medium">
+        <Text>Copyright Valou</Text>
+        <Anchor label="About" />
+      </Footer>
 
     </div>
   );
