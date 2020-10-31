5d9cb06727d3f620ffa38231d5041fd10c49491a
Valentin <parlier.valentin@gmail.com>
2020-10-31 15:54:28 +0100

    Ajout CV

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,7 @@ import { Home, UserFemale, Instagram, Twitter, Linkedin, Wifi, System, User, Tas
 import ynov_campus_logo from "./images/ynov_campus_logo.svg";
 import iut_blagnac_logo from "./images/iut_blagnac_logo.png";
 import durzy_logo from "./images/durzy_logo.png";
+import airbus_logo from "./images/airbus_logo.png";
 
 
 
@@ -20,21 +21,21 @@ const data = [
     icon: <Send size="large" />,
     title: 'Airbus',
     subTitle: 'Développeur',
-    message: 'Alternance',
+    message: 'Alternance de 3 ans',
   },
   {
     color: 'red',
     icon: <HostMaintenance size="large" />,
     title: 'Pmetique45',
-    subTitle: 'Technicien réseaux',
-    message: 'Stage',
+    subTitle: 'Installation / Supervision de serveurs (Centreon)',
+    message: 'Stage de 6 semaines',
   },
   {
     color: 'orange',
     icon: <Action size="large" />,
     title: 'Orange',
     subTitle: 'Technicien réseaux',
-    message: 'Stage',
+    message: 'Stage de 3 semaines',
   },
 ];
 
@@ -80,11 +81,13 @@ function App() {
   return (
     <div className="App">
       <MyHeader/>
+      <Box background="#2c3e50" pad="small"><Heading margin="auto">Bienvenue</Heading></Box>
       <Description/>
-      <Box background="#2c3e50" pad="small"><Heading margin="auto">Compétences</Heading></Box>
-      <Skills/>
       <Box  background="#2c3e50" pad="small"><Heading margin="auto">Experiences professionnelles</Heading></Box>
       <ProExp/>
+      <Box background="#2c3e50" pad="small"><Heading margin="auto">Compétences</Heading></Box>
+      <Skills/>
+
       <Box  background="#2c3e50" pad="small"><Heading margin="auto">Formations</Heading></Box>
       <Formations/>
       <ContactMe/>
@@ -96,12 +99,14 @@ function App() {
 function MyHeader() {
   return(
     <Header background="#2c3e50">
-      <Button  icon={<Avatar  size="5em" src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-19/s150x150/69396857_519805882155745_1325905225168453632_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_ohc=MuH0ZKhrlkoAX9MAy8p&oh=5d9fb64cd8b3765172e7f5d56538c6f9&oe=5FB1D1DF" />} hoverIndicator />
-      <Heading responsive="false" size="xsmall" textAlign="start" >Valentin Parlier</Heading>
+      <Box direction="row">
+        <Button  icon={<Avatar  size="5em" src="https://scontent-cdg2-1.cdninstagram.com/v/t51.2885-19/s150x150/69396857_519805882155745_1325905225168453632_n.jpg?_nc_ht=scontent-cdg2-1.cdninstagram.com&_nc_ohc=MuH0ZKhrlkoAX9MAy8p&oh=5d9fb64cd8b3765172e7f5d56538c6f9&oe=5FB1D1DF" />} hoverIndicator />
+        <Heading responsive="false" size="xsmall" textAlign="start" >Valentin Parlier</Heading>
+      </Box>
 
       <Nav direction="row">
-        <Anchor color="#2980b9" size="large" label="Accueil" href="#" />
-        <Anchor color="#2980b9" size="large" label="Mon CV" href="#" />
+        <Anchor color="#2980b9" size="large" label="Accueil" href="/" />
+        <Anchor color="#2980b9" size="large" label="Mon CV" href="ValentinParlier_CV.pdf" download />
         <Anchor color="#2980b9" size="large" label="Me contacter" href="#" />
       </Nav>
 
@@ -113,11 +118,10 @@ function Description() {
   return(
     <Box
       direction="row"
-      border={{ color: '#2c3e50', size: 'xlarge' }}
-      pad="medium"
+      pad="large"
+      background="#bdc3c7"
     >
-      <Box pad="medium" background="#bdc3c7">Valentin Parlier</Box>
-      <Box pad="medium" background="light-3">Webmaster sur Toulouse
+      <Box margin="2px" round="5px" pad="medium" background="light-3">Webmaster sur Toulouse
 Passionné du Web et des nouvelles technologies depuis des années, je me présente aujourd’hui à vous en tant que Webmaster. Un métier qui me permet de mettre à profit mes compétences de développeur web, de graphiste et d’intégrateur.</Box>
     </Box>
   )
@@ -139,10 +143,10 @@ function Skills() {
                 primaryKey="name"
                 secondaryKey="percent"
                 data={[
-                  { name: 'HTMsL/CSS', percent: '20%' },
-                  { name: 'React', percent: '30%' },
-                  { name: 'Java', percent: 40 },
-                  { name: 'Python', percent: 80 },
+                  { name: 'HTML/CSS' },
+                  { name: 'React'},
+                  { name: 'Java'},
+                  { name: 'Python'},
                 ]}
                 />
             </Box>
@@ -156,7 +160,7 @@ function Skills() {
               data={[
                 { name: 'Configuration d\'un réseau IPV4 et IPV6' },
                 { name: 'Mise en place de VLAN' },
-                { name: 'Installation de serveur' },
+                { name: 'Installation de serveurs' },
                 { name: 'Supervision de serveurs' },
               ]}
               />
@@ -219,9 +223,9 @@ function Formations() {
             >
             <Box height="small" width="medium" overflow="hidden" margin="auto" textAlign="center">
               <Carousel fill>
-              <Box><Text>Ynov Campus</Text><Image margin="auto" fit="cover" width="200px" src={ynov_campus_logo} /></Box>
-              <Box><Text>LP Pro APSIO IUT Blagnac</Text><Image margin="auto" fit="cover" width="200px" src={iut_blagnac_logo} /></Box>
-              <Box><Text>BTS SNIR</Text><Image margin="auto" fit="cover" width="200px" src={durzy_logo} /></Box>
+              <Box><Text>Mastère Dev web</Text><Image margin="auto" fit="cover" width="200px" src={ynov_campus_logo} /><Text>2020 - 2022</Text></Box>
+              <Box><Text>LP Pro APSIO</Text><Image margin="auto" fit="cover" width="200px" src={iut_blagnac_logo} /><Text>2019 - 2020</Text></Box>
+              <Box><Text>BTS SNIR</Text><Image margin="auto" fit="cover" width="200px" src={durzy_logo} /><Text>2017 - 2019</Text></Box>
               </Carousel>
             </Box>
         </Box>
@@ -247,5 +251,7 @@ function MyFooter() {
     </Footer>
   )
 }
+
+
 
 export default App;
